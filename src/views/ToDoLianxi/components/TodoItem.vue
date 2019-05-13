feec97106a5ab2dbfaa88a8315c2717970cbd708
常珂珂 <changkeke@sinosoft.com.cn>
2019-05-13 13:54:32 +0800

    todo拆组件（2）    鼠标事件

--- src/views/ToDoLianxi/components/TodoItem.vue
+++ src/views/ToDoLianxi/components/TodoItem.vue
@@ -1,10 +1,10 @@
 <template>
-    <li>
+    <li @mouseenter="handelColor(true)" @mouseleave="handelColor(false)" :style="{background: bgColor}">
         <label>
         <input type="checkbox" v-model="content.comeplete" />
         <span>{{content.title}}</span>
         </label>
-        <button class="btn btn-danger" style="display:none">删除</button>
+        <button class="btn btn-danger" v-show="isShow">删除</button>
     </li>
 
 </template>
@@ -15,9 +15,25 @@
             content: Object,
             index: Number
         },
-
+        data() {
+          return{
+              bgColor:'white',
+              isShow: false
+          }
+        },
         mounted() {
             console.log(this.content)
+        },
+        methods: {
+            handelColor(val){
+                if(val){
+                    this.bgColor='#aaaaaa'
+                    this.isShow=true
+                }else{
+                    this.bgColor='white'
+                    this.isShow=false
+                }
+            }
         }
     }
 
@@ -25,5 +41,39 @@
 
 
 <style scoped>
+    /*item*/
+    li {
+        list-style: none;
+        height: 36px;
+        line-height: 36px;
+        padding: 0 5px;
+        border-bottom: 1px solid #ddd;
+    }
+
+    li label {
+        float: left;
+        cursor: pointer;
+    }
+
+    li label li input {
+        vertical-align: middle;
+        margin-right: 6px;
+        position: relative;
+        top: -1px;
+    }
+
+    li button {
+        float: right;
+        /*display: none;*/
+        margin-top: 3px;
+    }
+
+    li:before {
+        content: initial;
+    }
+
+    li:last-child {
+        border-bottom: none;
+    }
 
 </style>
