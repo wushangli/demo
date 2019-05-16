6828f7bd3316ed0c975e63f43b5cf250cd086e64
常珂珂 <changkeke@sinosoft.com.cn>
2019-05-16 17:02:29 +0800

    案例_初始化显示（2）    axios与pubsub联合使用    兄弟组件传值使用pubsub      1)publish()方法发布消息      2)subscribe()方法订阅消息

--- src/views/users/components/Main.vue
+++ src/views/users/components/Main.vue
@@ -11,22 +11,68 @@
                 <a href="user.url" target="_blank">
                     <img src="user.vv_html" style='width: 100px'/>
                 </a>
-                <p class="card-text">user.name</p>
+                <p class="card-text">{{user.name}}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import PubSub from 'pubsub-js'
+    import axios from 'axios'
+
     export default {
         name: "Main",
         data() {
             return {
                 firstView:false,
+                frist:'false',
                 loading:false,
                 errmsg:'',
-                users:null
+                users: null
+
             }
+        },
+        watch: {
+            firstView:  {
+                handler: function (val) {
+                    console.log(222)
+                    console.log(val)
+                }
+            }
+        },
+        mounted(){
+           //发布消息
+           PubSub.subscribe('searchs',(msg,searchName) =>{
+               const searchurl = 'https://api.github.com/search/users?q='+searchName
+                //更新状态
+               this.firstView = true
+               this.frist = 'true'
+               this.loading = true
+               this.users = null
+               this.errmsg = ''
+               //获取数据
+                axios.get(searchurl).then(response =>{
+
+                    const result = response.data
+                    console.log(result)
+                    const users = result.items.map(item =>(
+                        {
+                            user:item.html_url,
+                            vv_html:item.avatar_url,
+                            name:item.login
+                        }))
+
+                    //成功
+                    this.loading=false
+                    this.users=users
+                    console.log(this.users)
+                    }).catch(error => {
+                        //失败
+                        this.loading=false
+                        this.errmsg='请求失败'
+                })
+           })
         }
     }
 </script>
--- src/views/users/components/Search.vue
+++ src/views/users/components/Search.vue
@@ -1,16 +1,28 @@
 <template>
     <section class="jumbotron">
-        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <h3 class="jumbotron-heading" >Search Github Users</h3>
         <div>
-            <input type="text" placeholder="enter the name you search"/>
-            <button>Search</button>
+            <input type="text" placeholder="enter the name you search" v-model="searchName"/>
+            <button @click="search">Search</button>
         </div>
     </section>
 </template>
 
 <script>
+    import PubSub from 'pubsub-js'
     export default {
-        name: "Search"
+        name: "Search",
+        data(){
+            return{
+                searchName:''
+            }
+        },
+        methods: {
+            search(){
+                //发布订阅消息
+                PubSub.publish('searchs',this.searchName)
+            }
+        }
     }
 </script>
 
